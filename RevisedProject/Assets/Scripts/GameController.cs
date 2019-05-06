14f23b296bec404bebcd1441810cc813f68fb354
Tobias Mache <tobyma23@gmail.com>
2019-05-06 11:18:45 -0700

    class 5/6

--- RevisedProject/Assets/Scripts/GameController.cs
+++ RevisedProject/Assets/Scripts/GameController.cs
@@ -7,14 +7,27 @@ using UnityEngine.Tilemaps;
 public class GameController : MonoBehaviour
 {
     bool gamestart = false;
+    bool optionsp = false;
+    bool backs = false;
     public string[] Levels;
 
     public Canvas MainMenuCanvas;
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
 
     public void PlayGame()
     {
         gamestart = true;
+    }
 
+    public void OptionsPressed()
+    {
+        optionsp = true;
+    }
+
+    public void BackPressed()
+    {
+        backs = true;
     }
 
     public void SceneChange(int lc)
@@ -39,6 +52,18 @@ public class GameController : MonoBehaviour
         if (gamestart == true)
         {
             MainMenuCanvas.GetComponent<Canvas>().enabled = false;
+        }
+        if (optionsp == true)
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+        if (backs == true)
+        {
+            optionsp = false;
+            backs = false;
+            optionsMenu.SetActive(false);
+            mainMenu.SetActive(true);  
         }
     }
 }
