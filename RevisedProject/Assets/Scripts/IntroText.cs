8e108239f4be20aa0d37c7969876a0778a5b4576
Rij Dorfman <rdorfman419@outlook.com>
2019-05-10 11:08:07 -0700

    Open scene works

---
+++ RevisedProject/Assets/Scripts/IntroText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroText : MonoBehaviour
+{
+
+    public string[] dialouge;
+    int counter = 0;
+    public Text talking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("z") && counter<=6)
+        {
+            counter++;
+            talking.text = dialouge[counter];
+        }
+
+    }
+
+
+
+
+}
